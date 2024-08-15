985e8326be1cf677ba7e3530e3825fecae3b27ce
atsu1125 <atsuchan@atsuchan.page>
2024-08-15 21:49:31 +0900

    fix: password reset API to delete expired requests

--- packages/backend/src/server/api/endpoints/reset-password.ts
+++ packages/backend/src/server/api/endpoints/reset-password.ts
@@ -1,4 +1,5 @@
 import bcrypt from 'bcryptjs';
+import ms from 'ms';
 import { publishMainStream } from '@/services/stream.js';
 import { Users, UserProfiles, PasswordResetRequests } from '@/models/index.js';
 import define from '../define.js';
@@ -11,8 +12,17 @@ export const meta = {
 
 	description: 'Complete the password reset that was previously requested.',
 
-	errors: {
+	limit: {
+		duration: ms('1hour'),
+		max: 3,
+	},
 
+	errors: {
+		noSuchResetRequest: {
+			message: 'No such reset request.',
+			code: 'NO_SUCH_RESET_REQUEST',
+			id: '6382759d-294c-43de-89b3-4e825006ca43',
+		},
 	},
 } as const;
 
@@ -27,13 +37,19 @@ export const paramDef = {
 
 // eslint-disable-next-line import/no-default-export
 export default define(meta, paramDef, async (ps, user) => {
-	const req = await PasswordResetRequests.findOneByOrFail({
+	const req = await PasswordResetRequests.findOneBy({
 		token: ps.token,
 	});
+	if (req == null) throw new ApiError(meta.errors.noSuchResetRequest);
 
 	// 発行してから30分以上経過していたら無効
+	// expires after 30 minutes
+	// This is a secondary check just in case the expiry task is broken,
+	// the expiry task is badly aligned with this expiration or something
+	// else strange is going on.
 	if (Date.now() - req.createdAt.getTime() > 1000 * 60 * 30) {
-		throw new Error(); // TODO
+		await PasswordResetRequests.delete(req.id);
+		throw new ApiError(meta.errors.noSuchResetRequest);
 	}
 
 	// Generate hash of password
@@ -44,5 +60,5 @@ export default define(meta, paramDef, async (ps, user) => {
 		password: hash,
 	});
 
-	PasswordResetRequests.delete(req.id);
+	await PasswordResetRequests.delete(req.id);
 });
