e1118c1485a04eb6457cbabac9e4a99fb0902dde
atsu1125 <atsuchan@atsuchan.page>
2024-08-28 11:55:52 +0900

    fix: delete account who has Emoji files

--- packages/backend/src/services/drive/delete-file.ts
+++ packages/backend/src/services/drive/delete-file.ts
@@ -14,7 +14,10 @@ export async function deleteFile(file: DriveFile, isExpired = false) {
 			publicUrl: file.webpublicUrl,
 		});
 		if (emojis != null) {
-			return; // emojiのpublicUrlがfileに含まれている場合は処理をスキップ
+			await DriveFiles.update({ id: file.id }, {
+				user: null,
+			});
+			return; // emojiのpublicUrlがfileに含まれている場合は所有ユーザーをnullにして終了
 		}
 	} else if (file.url != null) {
 		let emojis = await Emojis.findOneBy({
@@ -22,7 +25,10 @@ export async function deleteFile(file: DriveFile, isExpired = false) {
 			publicUrl: file.url,
 		});
 		if (emojis != null) {
-			return; // emojiのpublicUrlがfileに含まれている場合は処理をスキップ
+			await DriveFiles.update({ id: file.id }, {
+				user: null,
+			});
+			return; // emojiのpublicUrlがfileに含まれている場合は所有ユーザーをnullにして終了
 		}
 	}
 
@@ -58,7 +64,10 @@ export async function deleteFileSync(file: DriveFile, isExpired = false) {
 			publicUrl: file.webpublicUrl,
 		});
 		if (emojis != null) {
-			return; // emojiのpublicUrlがfileに含まれている場合は処理をスキップ
+			await DriveFiles.update({ id: file.id }, {
+				user: null,
+			});
+			return; // emojiのpublicUrlがfileに含まれている場合は所有ユーザーをnullにして終了
 		}
 	} else if (file.url != null) {
 		let emojis = await Emojis.findOneBy({
@@ -66,7 +75,10 @@ export async function deleteFileSync(file: DriveFile, isExpired = false) {
 			publicUrl: file.url,
 		});
 		if (emojis != null) {
-			return; // emojiのpublicUrlがfileに含まれている場合は処理をスキップ
+			await DriveFiles.update({ id: file.id }, {
+				user: null,
+			});
+			return; // emojiのpublicUrlがfileに含まれている場合は所有ユーザーをnullにして終了
 		}
 	}
 
