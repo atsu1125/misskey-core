069335de36ac8a030caa970b7faca70779f53cdd
atsu1125 <atsuchan@atsuchan.page>
2025-07-29 14:03:24 +0900

    feat(backend): Hide Silenced User Notes from Public Timelines

--- packages/backend/src/server/api/endpoints/notes/local-timeline.ts
+++ packages/backend/src/server/api/endpoints/notes/local-timeline.ts
@@ -9,7 +9,7 @@ import { generateVisibilityQuery } from '../../common/generate-visibility-query.
 import { generateRepliesQuery } from '../../common/generate-replies-query.js';
 import { generateMutedNoteQuery } from '../../common/generate-muted-note-query.js';
 import { generateBlockedUserQuery } from '../../common/generate-block-query.js';
-import { generateSuspendedUserQueryForNote } from '../../common/generate-suspended-query.js';
+import { generateSilencedUserQueryForNote, generateSuspendedUserQueryForNote } from '../../common/generate-suspended-query.js';
 
 export const meta = {
 	tags: ['notes'],
@@ -82,6 +82,7 @@ export default define(meta, paramDef, async (ps, user) => {
 	generateRepliesQuery(query, user);
 	generateVisibilityQuery(query, user);
 	generateSuspendedUserQueryForNote(query);
+	generateSilencedUserQueryForNote(query);
 	if (user) generateMutedUserQuery(query, user);
 	if (user) generateMutedNoteQuery(query, user);
 	if (user) generateBlockedUserQuery(query, user);
